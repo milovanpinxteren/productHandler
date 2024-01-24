d51403b0a447568411d680cedd10f5e955300cab
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-01-24 17:34:45 +0100

    end_of_day: make and storing of image

--- main/main.py
+++ main/main.py
@@ -1,16 +1,12 @@
-from barcode.barcode_scanner import BarcodeScanner
 from barcode.user_interface.user_interface import UserInterface
 
 
 
 
 def main():
-    # Create an instance of BarcodeScanner
-    # scanner = BarcodeScanner()
     user_inferface = UserInterface()
+    print('user interface init')
     user_inferface.run()
-    # Start the barcode scanning loop
-    # scanner.scan()
 
 if __name__ == '__main__':
     main()
