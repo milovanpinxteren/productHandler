7940cfb1e48a426e9fce33261b5b9de939166628
Milo <milo.van.pinxteren@gmail.com>
2025-08-08 11:22:33 +0200

    feat: add glazen to lists.py

--- main/data/lists.py
+++ main/data/lists.py
@@ -118,7 +118,7 @@ class Lists:
                          'Lambiek/Geuze', 'Medebier', 'Pilsner', 'Porter', 'Quadrupel', 'Stout', 'Sour', 'Overig',
                          'Weizen', 'Witbier', 'Alcoholvrij', 'Amber', 'Cider', 'Ijsbock', 'Ale', 'Session',
                          'Cadeaupakket',
-                         'DIPA', 'TIPA', 'QIPA', 'NEIPA', 'Champagnebier', 'Wild Ale', 'Wijn']
+                         'DIPA', 'TIPA', 'QIPA', 'NEIPA', 'Champagnebier', 'Wild Ale', 'Wijn', 'Glazen']
 
     aging_methods = ['Barrel Aged', 'Niet Barrel Aged']
 
